c27074a83d8b126888ca50620d05a0286c1aaed7
wally1997 <walterbolivar97@gmail.com>
2023-02-23 22:23:27 -0500

    layouts route, partials

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from "express";
-import indexRoutes from "./routes/index.routes"
+import indexRoutes from "./routes/index.routes";
+import {create} from "express-handlebars";
+import path from "path";
 
 const app = express();
 
-app.use(indexRoutes)
+//template engine, line 9 where see views
+app.set("views", path.join(__dirname, "views"));
+
+const hbs = create({
+    
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaultLayout:'main',
+    extname: '.hbs',
+  });
+app.engine(".hbs", hbs.engine, );
+app.set("view engine", ".hbs");
+
+// Routes
+app.use(indexRoutes);
 
 export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 
 const router = Router();
+
+/* For new routes, just coppy and modify the code below */
+
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
+
+router.get("/about", (req, res) => {
+  res.send("about");
+});
+
+
+
+
+
 
 export default router;
